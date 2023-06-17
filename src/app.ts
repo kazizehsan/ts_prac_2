c033ff3d2f1a545fde1de148a634cf7367d4303e
Kazi Ehsan Aziz <kaziehsanaziz@Kazis-MacBook-Pro.local>
2023-06-17 15:41:57 +0600

    eslint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import http from "http";
 import { serverName } from "./server-name";
 
-const hostname: string = "localhost";
-const port: number = 3000;
+const hostname = "localhost";
+const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
